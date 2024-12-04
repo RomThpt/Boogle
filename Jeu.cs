686551f73f6aca0848bf9d3ed939a3a68e809790
HugoYahh <clauzoh@gmail.com>
2024-12-04 23:38:50 +0100

    Modif De Classe
    Début de la classe Jeu et Plateau, et des fichiers txt du sujet.
    Dans la classe Plateau, reste à faire la méthode GénererDes, pour attribuer à chaque face une lettre en tenant compte de la fréquence maximale autorisé (pas si simple)
    Dans Jeu, faut encore faire tout l'affichage et le visuel j'ai juste défini la classe.

---
+++ Jeu.cs
@@ -0,0 +1,22 @@
+public class Jeu
+{
+    public List<Joueur> joueurs;
+    public Plateau plateauactuel;
+    public Dictionnaire dico;
+
+    public Jeu(List<Joueur> joueurs, Plateau plateauactuel, Dictionnaire dico)
+    {
+        this.joueurs = joueurs;
+        this.plateauactuel = plateauactuel;
+        this.dico = dico;   
+    }
+
+    public List<JoueurJoueur> Joueurs {get {return joueurs;} set{} }
+    public Plateau Plateauactuel {get {return plateauactuel;} set{} }
+    public Dictionnaire Dico {get {return dico;} set{} }
+
+    public void ConfigJeu ()
+    {
+        
+    }
+}
--- Plateau.cs
+++ Plateau.cs
@@ -1,1 +1,60 @@
+public class Plateau
+{
+    public Dé[,] des ;
 
+    public Dé[,] Des {get ; private set; }
+
+    public Plateau(int taille, Dictionnary <char,int> lettreFrequencies)
+    {
+        List<De> desDisponibles = GenererDes(lettreFrequencies);
+        Des = new De[taille, taille];
+
+        Random random = new Random();
+        var desMelanges = desDisponibles.OrderBy(x => random.Next()).ToArray();
+
+        int index = 0;
+        for (int i = 0; i < taille; i++)
+        {
+            for (int j = 0; j < taille; j++)
+            {
+                Des[i, j] = desMelanges[index++];
+                Des[i, j].Lance(random); // Lancer le dé pour définir la face visible
+            }
+        }
+    }
+
+
+
+
+    private List<De> GenererDes(Dictionnary<char, int> lettreFrequencies)  
+    {
+        List<De> des = new List<De>();
+        Random random = new Random();
+
+        
+    } 
+
+    public string ToString()
+    {
+        string s = "";
+        if (Des != null && Des.GetLength(0) > 0 && Des.GetLength(1) > 0)
+        {
+            for (int i = 0; i < Des.GetLength(0); i++)
+            {
+                for (int j = 0; j < Des.GetLength(1); j++)
+                {
+                    // Ajoute directement la face visible du dé
+                    s += char.ToUpper(Des[i, j].FaceVisible) + " ";
+                }
+                s += "\n"; // Saut de ligne après chaque rangée
+            }
+        }
+        else
+        {
+            s = "Erreur, le plateau de jeu n'est pas défini correctement";
+        }
+
+        return s; // Retourne la chaîne formée
+    }
+
+}
