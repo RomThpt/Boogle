6b3836092b4916f927ba780d888a18574a6e9713
boulougarou <clerc073@gmail.com>
2024-12-05 09:32:03 +0100

    correction erreur

--- De.cs
+++ De.cs
@@ -1,29 +1,34 @@
 using System;
-public class De
+namespace boogle
 {
-    private char[] faces;
-    private char visibleFace;
 
-    public De(char[] faces)
-    {
-        if (faces.Length != 6)
-            throw new ArgumentException("Un dé doit avoir exactement 6 faces.");
-        this.faces = faces;
-        Lance(new Random());
-    }
 
-    public void Lance(Random r)
+    public class De
     {
-        visibleFace = faces[r.Next(6)];
-    }
+        private char[] faces;
+        private char visibleFace;
 
-    public override string ToString()
-    {
-        return $"Dé: {string.Join(", ", faces)}, Face visible: {visibleFace}";
-    }
+        public De(char[] faces)
+        {
+            if (faces.Length != 6)
+                throw new ArgumentException("Un dé doit avoir exactement 6 faces.");
+            this.faces = faces;
+            Lance(new Random());
+        }
 
-    public char VisibleFace
-    {
-        get { return visibleFace; }
+        public void Lance(Random r)
+        {
+            visibleFace = faces[r.Next(6)];
+        }
+
+        public override string ToString()
+        {
+            return $"Dé: {string.Join(", ", faces)}, Face visible: {visibleFace}";
+        }
+
+        public char VisibleFace
+        {
+            get { return visibleFace; }
+        }
     }
 }
