b9d6ffe70288a21e1fd3b319ddb7d31d815cef97
boulougarou <clerc073@gmail.com>
2024-12-05 09:49:12 +0100

    correction erreur plateau

--- Plateau.cs
+++ Plateau.cs
@@ -1,60 +1,64 @@
-public class Plateau
+namespace boogle
 {
-    public Dé[,] des ;
-
-    public Dé[,] Des {get ; private set; }
-
-    public Plateau(int taille, Dictionnary <char,int> lettreFrequencies)
+ 
+    public class Plateau
     {
-        List<De> desDisponibles = GenererDes(lettreFrequencies);
-        Des = new De[taille, taille];
+        public De[,] des ;
 
-        Random random = new Random();
-        var desMelanges = desDisponibles.OrderBy(x => random.Next()).ToArray();
+        public De[,] Des {get ; private set; }
 
-        int index = 0;
-        for (int i = 0; i < taille; i++)
+        public Plateau(int taille, Dictionary <char,int> lettreFrequencies)
         {
-            for (int j = 0; j < taille; j++)
+            List<De> desDisponibles = GenererDes(lettreFrequencies);
+            Des = new De[taille, taille];
+
+            Random random = new Random();
+            var desMelanges = desDisponibles.OrderBy(x => random.Next()).ToArray();
+
+            int index = 0;
+            for (int i = 0; i < taille; i++)
             {
-                Des[i, j] = desMelanges[index++];
-                Des[i, j].Lance(random); // Lancer le dé pour définir la face visible
-            }
-        }
-    }
-
-
-
-
-    private List<De> GenererDes(Dictionnary<char, int> lettreFrequencies)  
-    {
-        List<De> des = new List<De>();
-        Random random = new Random();
-
-        
-    } 
-
-    public string ToString()
-    {
-        string s = "";
-        if (Des != null && Des.GetLength(0) > 0 && Des.GetLength(1) > 0)
-        {
-            for (int i = 0; i < Des.GetLength(0); i++)
-            {
-                for (int j = 0; j < Des.GetLength(1); j++)
+                for (int j = 0; j < taille; j++)
                 {
-                    // Ajoute directement la face visible du dé
-                    s += char.ToUpper(Des[i, j].FaceVisible) + " ";
+                    Des[i, j] = desMelanges[index++];
+                    Des[i, j].Lance(random); // Lancer le dé pour définir la face visible
                 }
-                s += "\n"; // Saut de ligne après chaque rangée
             }
         }
-        else
+
+
+
+
+        private List<De> GenererDes(Dictionary<char, int> lettreFrequencies)  
         {
-            s = "Erreur, le plateau de jeu n'est pas défini correctement";
+            List<De> des = new List<De>();
+            Random random = new Random();
+
+            return null;
+        } 
+
+        public string ToString()
+        {
+            string s = "";
+            if (Des != null && Des.GetLength(0) > 0 && Des.GetLength(1) > 0)
+            {
+                for (int i = 0; i < Des.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Des.GetLength(1); j++)
+                    {
+                        // Ajoute directement la face visible du dé
+                        s += char.ToUpper(Des[i, j].VisibleFace) + " ";
+                    }
+                    s += "\n"; // Saut de ligne après chaque rangée
+                }
+            }
+            else
+            {
+                s = "Erreur, le plateau de jeu n'est pas défini correctement";
+            }
+
+            return s; // Retourne la chaîne formée
         }
 
-        return s; // Retourne la chaîne formée
     }
-
 }
